567d68fa2692c84f5ba6c14d2600bba2525c37a9
justinchang91 <justin.l.chang@hotmail.com>
2020-05-26 14:44:14 -0400

    pretty much finished the screen thing. Next step is to work on creating a GUI

--- loginscreen/login.py
+++ loginscreen/login.py
@@ -4,39 +4,90 @@
 import csv
 
 
-class User:
-    """
-    Class that offers the user to login or to create a new account
-    """
-
-    def __init__ (self):
-        self.username = ""
-        self.password = ""
-        self.security_question = ""
-        self.security_answer = ""
-
-    def create_account(self, username, password, security_question, security_answer):
-        self.username = username
-        self.password = password
-        self.security_question = security_question
-        self.security_answer = security_answer
-
-        user_info = [self.username, self.password, self.security_question, self.security_answer]
-        myfile = open("users.csv", "w")
-        enter_info = csv.writer(myfile)
-        enter_info.writerow(user_info)
-
-    def login(self):
-        pass
-
-    def ask_security_question(self):
-        pass
+def create_account(username, password, security_question, security_answer):
+    user_info = [username, password, security_question, security_answer]
+    myfile = open("users.csv", "a")
+    enter_info = csv.writer(myfile)
+    enter_info.writerow(user_info)
+    myfile.close()
 
 
-person1 = User()
-username = input("Enter username: ")
-password = input("Enter password: ")
-security_question = input("Enter security question: ")
-security_answer = input("Enter answer to security quesetion: ")
+def login(username, password):
+    # Read the csv file to see if the user and password are in it.
+    myfile = open("users.csv", "r")
+    user_reader = list(csv.reader(myfile))
+    myfile.close()
 
-person1.create_account(username, password, security_question, security_answer)
+    found = False
+    row_count = 0
+    while not found:
+        if user_reader[row_count][0] == username and user_reader[row_count][1] == password:
+            return True
+
+        elif row_count == len(user_reader)-1:
+            return False
+
+        else:
+            row_count += 1
+
+
+def find_account(username):
+    myfile = open("users.csv", "r")
+    user_reader = list(csv.reader(myfile))
+    myfile.close()
+
+    # Find the user in the database:
+    found = False
+    row_count = 0
+    while not found:
+        if user_reader[row_count][0] == username:
+            return user_reader[row_count]
+        elif row_count == len(user_reader)-1:
+            return False
+        else:
+            row_count += 1
+
+
+def main():
+    print("Welcome!")
+
+    action = input("Press 1 to login \nPress 2 to create an account \nPress 3 if you forgot your password")
+
+    # For login
+    if action == "1":
+        again = True
+        while again:
+            user = input("Enter username: ")
+            pw = input("Enter password: ")
+            if login(user, pw):
+                print("Successful login!")
+                again = False
+            else:
+                go_again = input("User not found in database. Try again? (y/n)")
+                if go_again != "y":
+                    again = False
+
+    # To create account
+    if action == "2":
+        user = input("Enter username: ")
+        pw = input("Enter password: ")
+        sq = input("Enter a security question: ")
+        sa = input("Enter the answer to the security question: ")
+        create_account(user, pw, sq, sa)
+        print("Account created!")
+
+    # If password is forgotten
+    if action == "3":
+        user = input("Enter your username: ")
+        if find_account(user):
+            info = find_account(user)
+            print("Please answer the following question:")
+            user_answer = input(info[2])
+            if user_answer == info[3]:
+                print("Your password is " + info[1])
+        else:
+            print("Username not found in database.")
+
+
+main()
+
